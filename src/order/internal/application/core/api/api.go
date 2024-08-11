3a2ddfd78098a9029add7d1c593ef4edef91d808
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-08-11 15:26:03 +0400

    Update

--- src/order/internal/application/core/api/api.go
+++ src/order/internal/application/core/api/api.go
@@ -1,8 +1,12 @@
 package api
 
 import (
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"grpc-microservices-go/order/internal/application/core/domain"
 	"grpc-microservices-go/order/internal/ports"
+	"strings"
 )
 
 type Application struct {
@@ -18,13 +22,31 @@ func NewApplication(db ports.DbPort, payment ports.PaymentPort) *Application {
 }
 
 func (app Application) PlaceOrder(order domain.Order) (domain.Order, error) {
-	err := app.db.Save(order)
+	err := app.db.Save(&order)
 	if err != nil {
 		return domain.Order{}, err
 	}
 	paymentErr := app.payment.Charge(&order)
 	if paymentErr != nil {
-		return domain.Order{}, paymentErr
+		st := status.Convert(paymentErr)
+		var allErrors []string
+		for _, detail := range st.Details() {
+			switch t := detail.(type) {
+			case *errdetails.BadRequest:
+				for _, violation := range t.GetFieldViolations() {
+					allErrors = append(allErrors, violation.Description)
+				}
+			}
+		}
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: strings.Join(allErrors, "\n"),
+		}
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+		statusWithDetails, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDetails.Err()
 	}
 
 	return order, nil
--- src/order/internal/ports/db.go
+++ src/order/internal/ports/db.go
@@ -4,5 +4,5 @@ import "grpc-microservices-go/order/internal/application/core/domain"
 
 type DbPort interface {
 	Get(id string) (domain.Order, error)
-	Save(domain.Order) error
+	Save(*domain.Order) error
 }
